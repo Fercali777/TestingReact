08279088e293c28ee1a1c55e80e5e002a6708221
Fercali777 <fernandocalixto89@gmail.com>
2025-03-10 11:34:00 +0100

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+function footer() {
+    return (
+        <footer>
+            All rights reserved
+        </footer>
+    )
+}
+export default footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header () {
+return (
+<header>
+    <div className="logo">Logo</div>
+    <nav>
+        <a href="#" >Home</a>
+        <a href="#">Details</a>
+    </nav>
+</header>
+)
+
+}
+
+export default Header;
